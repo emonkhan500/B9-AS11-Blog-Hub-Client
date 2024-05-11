8a146359b690cbd1ac577c215996c08710b1ab1f
emonkhan500 <khdilo47@gmail.com>
2024-05-11 15:21:37 +0600

    env file added

--- src/Components/Firebase/Firebase.config.js
+++ src/Components/Firebase/Firebase.config.js
@@ -5,13 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA8W4wA8yt4XLdsGhn-XaDhQshQ80_b4_o",
-  authDomain: "b9-assignment-11.firebaseapp.com",
-  projectId: "b9-assignment-11",
-  storageBucket: "b9-assignment-11.appspot.com",
-  messagingSenderId: "1016693716155",
-  appId: "1:1016693716155:web:032cfd69af7146c905259e"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain:import.meta.env.VITE_authDomain ,
+    projectId:import.meta.env.VITE_projectId ,
+    storageBucket:import.meta.env.VITE_storageBucket,
+    messagingSenderId:import.meta.env.VITE_messagingSenderId ,
+    appId:import.meta.env.VITE_appId 
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export default app
