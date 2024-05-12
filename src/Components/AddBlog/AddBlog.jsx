1a43ac1f5ce3cbf60d175323e58ca4f7b681526f
emonkhan500 <khdilo47@gmail.com>
2024-05-12 13:09:59 +0600

    add blog page complete

--- src/Components/AddBlog/AddBlog.jsx
+++ src/Components/AddBlog/AddBlog.jsx
@@ -1,9 +1,83 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { AuthContext } from '../provider/AuthProvider';
+import { useNavigate } from 'react-router-dom';
 
 const AddBlog = () => {
+    useEffect(()=>{
+        document.title='Add Blog'
+    },[])
+
+    const{user}=useContext(AuthContext)
+const navigate=useNavigate()
+
+const handleAddBlog = (e) => {
+    e.preventDefault();
+    const image = e.target.image.value;
+    const title = e.target.title.value;
+    const category = e.target.category.value;
+    
+    const description = e.target.description.value;
+    const longdescription = e.target.longdescription.value;
+    
+    const blog = { image, title,category, description,longdescription};
+    console.log(blog);
+
+    // send data to server
+    // fetch('https://b9-assignment10-server-ten.vercel.app/spot',{
+    //     method:'POST',
+    //     headers:{
+    //         'content-type':'application/json'
+    //     },
+    //     body:JSON.stringify(place)
+    // })
+    // .then(res =>res.json())
+    // .then(data=>{
+    //     console.log(data)
+    //     if(data.insertedId){
+    //         Swal.fire({
+    //           title: 'Success!',
+    //           text: 'spot added successfully',
+    //           icon: 'success',
+    //           confirmButtonText: 'Cool'
+    //         })
+    //        navigate('/')
+    //       }
+    // })
+    
+  };
+
     return (
-        <div>
-            <h1>this is addblog</h1>
+        <div className="bg-gray-100 p-6 mt-11 md:p-12">
+            <h2 className="text-3xl font-extrabold text-center mb-8 text-cyan-300">Add a Blog</h2>
+            <form onSubmit={handleAddBlog}>
+                <div className="mb-8">
+                    <label className="block mb-2">Image URL</label>
+                    <input type="text" name="image" placeholder="Image URL" className="input input-bordered w-full" />
+                </div>
+                <div className="mb-8">
+                    <label className="block mb-2">Title</label>
+                    <input type="text" name="title" placeholder="Title" className="input input-bordered w-full" />
+                </div>
+                <div className="mb-8">
+                    <label className="block mb-2">Category</label>
+                    <select name="category" className="input input-bordered w-full">
+                        
+                        <option value="Fashion">Fashion</option>
+                        <option value="Travel">Travel</option>
+                        <option value="Food">Food</option>
+                       
+                    </select>
+                </div>
+                <div className="mb-8">
+                    <label className="block mb-2">Short Description</label>
+                    <input type="text" name="description" placeholder="Short Description" className="input input-bordered w-full" />
+                </div>
+                <div className="mb-8">
+                    <label className="block mb-2">Long Description</label>
+                    <input type="text" name="longdescription" placeholder="Long Description" className="input input-bordered w-full" />
+                </div>
+                <button type="submit" className="btn btn-block text-white btn-success">Submit</button>
+            </form>
         </div>
     );
 };
