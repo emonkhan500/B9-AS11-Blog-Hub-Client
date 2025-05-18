93df587dd75d92881bfbbf640829d8d8acfbbb11
emonkhan500 <khdilo47@gmail.com>
2025-05-19 00:06:42 +0600

    started working on banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div className='container mx-auto mt-12'>
+        <div className='container mx-auto md:mt-12'>
          <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -26,20 +26,20 @@ const Banner = () => {
         modules={[Pagination,Autoplay,Navigation]} className="mySwiper">
         <SwiperSlide><div className='slide slide1'>
            <div className='text-center'>
-           <h1 className='text-white font-bold text-5xl '> <span className='text-red-600'>Country </span> roads take me <br /> home</h1>
-           <button className="btn btn-success bg-[#0BE58A] w-[180px] mt-7 text-white text-xl font-semibold rounded-xl">Read More</button>
+           <h1 className='text-white font-bold text-2xl md:text-3xl lg:text-5xl '> <span className='text-red-600'>Country </span> roads take me <br /> home</h1>
+           <Link to='/allBlogs' className="btn btn-success bg-gradient-to-r from-green-500 to-green-600 w-[180px] mt-7 text-white text-xl font-semibold rounded-xl">Read More</Link>
            </div>
            <br />
            
         </div></SwiperSlide>
         <SwiperSlide><div className='slide slide2'> <div className='text-center'>
-           <h1 className='text-white font-bold text-5xl'>Love this <span className='text-red-600'>Morning </span> outside love this cup of <br /> coffee</h1>
-           <button className="btn btn-success bg-[#0BE58A] w-[180px] mt-7 text-white text-xl font-semibold rounded-xl">Read More</button>
+           <h1 className='text-white font-bold text-2xl md:text-3xl lg:text-5xl'>Love this <span className='text-red-600'>Morning </span> outside love this cup of <br /> coffee</h1>
+           <Link to='/allBlogs' className="btn btn-success bg-gradient-to-r from-green-500 to-green-600 w-[180px] mt-7 text-white text-xl font-semibold rounded-xl">Read More</Link>
            </div>
            <br /></div></SwiperSlide>
         <SwiperSlide><div className='slide slide3'> <div className='text-center'>
-           <h1 className='text-white font-bold text-5xl'>The story of <span className='text-red-600'>Alibaba</span> and his 40 bandits was my favorite</h1>
-           <button className="btn btn-success bg-[#0BE58A] w-[180px] mt-7 text-white text-xl font-semibold rounded-xl">Read More</button>
+           <h1 className='text-white font-bold text-2xl md:text-3xl lg:text-5xl'>The story of <span className='text-red-600'>Alibaba</span> and his 40 bandits was my favorite</h1>
+           <Link to='/allBlogs' className="btn btn-success bg-gradient-to-r from-green-500 to-green-600 w-[180px] mt-7 text-white text-xl font-semibold rounded-xl">Read More</Link>
            </div>
            <br /></div></SwiperSlide>
        
