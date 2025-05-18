ee226869e5c237e199725cc24b8bd5a413313643
emonkhan500 <khdilo47@gmail.com>
2025-05-19 00:58:07 +0600

    complete  working on recent blog

--- src/Components/RecentBlog/RecentBlog.jsx
+++ src/Components/RecentBlog/RecentBlog.jsx
@@ -10,11 +10,11 @@ useEffect(()=>{
 },[])                                 
 
     return (
-        <div className='mt-10'>
-<h1 className='text-center md:text-3xl lg:text-4xl font-bold mt-20  text-green-500'>These Are Recently Added BLog</h1>
-<hr  className='w-72 mx-auto mt-5 border-2 border-green-500'/>
+        <div className=''>
+<h1 className='text-center text-xl md:text-3xl lg:text-4xl font-bold mt-12  text-green-500'>These Are Recently Added BLog</h1>
+<hr  className='w-72 mx-auto mt-2 border-2 border-green-500'/>
 
-            <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 mt-5">
+            <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3  mt-2 md:px-8 lg:px-28">
                 {
                     blogs?.map(blog=><ShowRecentBlog blog={blog}></ShowRecentBlog>)
                 }
